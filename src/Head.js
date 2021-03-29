1d613c80d1b1fff3639ff55174cbcd545c2c1a9c
Michell Zipitría <mzipi@outlook.com>
2021-03-29 03:22:44 -0300

    actualizando desde la web

---
+++ src/Head.js
@@ -0,0 +1,17 @@
+import './head.css';
+
+function Head(){
+  return(
+    <div className="head">
+      <nav>
+        <ul>
+          <li><a href="#">INICIO</a></li>
+          <li><a href="#">PERFIL</a></li>
+          <li><a href="#">BLOG</a></li>
+          <li><a href="#">PORTFOLIO</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default Head;
---
+++ src/Main.js
@@ -0,0 +1,38 @@
+import './main.css';
+
+function Main(){
+  return(
+    <div className="body">
+      <main>
+        <div>
+          <article>
+            <h3>Titulo del post 1</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, nisi turpis fusce ridiculus sapien sem blandit, praesent potenti primis urna suspendisse cubilia. Gravida fames ligula convallis ridiculus suscipit elementum placerat feugiat per, nisi rhoncus dignissim fermentum sodales mollis quam mi nullam platea, habitant semper non montes tempus leo tempor tristique. Cras mi vestibulum arcu class praesent phasellus conubia ante netus, luctus natoque enim bibendum cursus aliquam massa ridiculus, magnis aenean primis dictumst curabitur magna consequat eget.
+
+Bibendum cursus ullamcorper neque purus morbi sem vestibulum, ultricies nisi feugiat condimentum lacus quis, justo porttitor mattis iaculis dis porta. Dictumst class penatibus convallis ultrices nec interdum commodo pretium ac, porttitor sodales neque nibh nostra tempor platea mollis, integer pharetra enim auctor ante scelerisque dictum facilisis. Nascetur nam lacus habitant malesuada viverra litora molestie neque, facilisis aliquam porta consequat posuere suspendisse.
+              </p>
+          </article>
+        </div>
+        <div>
+          <article>
+            <h3>Titulo del post 2</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, nisi turpis fusce ridiculus sapien sem blandit, praesent potenti primis urna suspendisse cubilia. Gravida fames ligula convallis ridiculus suscipit elementum placerat feugiat per, nisi rhoncus dignissim fermentum sodales mollis quam mi nullam platea, habitant semper non montes tempus leo tempor tristique. Cras mi vestibulum arcu class praesent phasellus conubia ante netus, luctus natoque enim bibendum cursus aliquam massa ridiculus, magnis aenean primis dictumst curabitur magna consequat eget.
+
+Bibendum cursus ullamcorper neque purus morbi sem vestibulum, ultricies nisi feugiat condimentum lacus quis, justo porttitor mattis iaculis dis porta. Dictumst class penatibus convallis ultrices nec interdum commodo pretium ac, porttitor sodales neque nibh nostra tempor platea mollis, integer pharetra enim auctor ante scelerisque dictum facilisis. Nascetur nam lacus habitant malesuada viverra litora molestie neque, facilisis aliquam porta consequat posuere suspendisse.
+              </p>
+          </article>
+        </div>
+        <div>
+          <article>
+            <h3>Titulo del post 3</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, nisi turpis fusce ridiculus sapien sem blandit, praesent potenti primis urna suspendisse cubilia. Gravida fames ligula convallis ridiculus suscipit elementum placerat feugiat per, nisi rhoncus dignissim fermentum sodales mollis quam mi nullam platea, habitant semper non montes tempus leo tempor tristique. Cras mi vestibulum arcu class praesent phasellus conubia ante netus, luctus natoque enim bibendum cursus aliquam massa ridiculus, magnis aenean primis dictumst curabitur magna consequat eget.
+
+Bibendum cursus ullamcorper neque purus morbi sem vestibulum, ultricies nisi feugiat condimentum lacus quis, justo porttitor mattis iaculis dis porta. Dictumst class penatibus convallis ultrices nec interdum commodo pretium ac, porttitor sodales neque nibh nostra tempor platea mollis, integer pharetra enim auctor ante scelerisque dictum facilisis. Nascetur nam lacus habitant malesuada viverra litora molestie neque, facilisis aliquam porta consequat posuere suspendisse.
+              </p>
+          </article>
+        </div>
+      </main>
+    </div>
+  )
+}
+export default Main;
