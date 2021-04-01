97d205bff3ee488cc7596b0282f89e67c7ffe7b7
Michell Zipitría <mzipi@outlook.com>
2021-04-01 01:12:16 -0300

    ads

--- src/Ads.js
+++ src/Ads.js
@@ -1,13 +1,17 @@
-import './test.js';
+import React from 'react';
 
-function Ads(){
-    return(
-      <ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-1276989055234826"
-     data-ad-slot="3586502935"></ins>
-    )
+export default class Ads extends React.Component {
+  componentDidMount () {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+render () {
+    return (
+        <ins className='adsbygoogle'
+          style={{ display: 'block' }}
+          data-ad-format="auto"
+          data-ad-client="ca-pub-1276989055234826"
+          data-ad-slot="3586502935" />
+    );
+  }
 }
-export default Ads;
