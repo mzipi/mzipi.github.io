fb24e6ea420ad03a1857978a3b3212a46851dab9
Michell Zipitría <mzipi@outlook.com>
2021-03-31 23:52:19 -0300

    ads

--- src/Ads.js
+++ src/Ads.js
@@ -2,7 +2,11 @@ import Ads2 from './Ads2';
 
 function Ads(){
     return(
-      <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1276989055234826" data-ad-slot="9577196219" data-ad-format="auto" data-full-width-responsive="true"></ins>
+      <ins class="adsbygoogle" style="display:block"
+      data-ad-client="ca-pub-1276989055234826"
+      data-ad-slot="9577196219"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
     )
 }
 export default Ads;
