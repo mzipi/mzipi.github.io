b68da50ed6b414f7170bee37bae1408bebc691ba
Michell Zipitría <mzipi@outlook.com>
2021-04-01 00:02:17 -0300

    ads

--- src/Ads.js
+++ src/Ads.js
@@ -1,12 +1,15 @@
-import Ads2 from './Ads2';
-
 function Ads(){
     return(
-      <ins class="adsbygoogle" style="display:block"
-      data-ad-client="ca-pub-1276989055234826"
-      data-ad-slot="9577196219"
-      data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
+      <div>
+        <ins class="adsbygoogle" style="display:block"
+        data-ad-client="ca-pub-1276989055234826"
+        data-ad-slot="9577196219"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+
+      </div>
     )
 }
+(adsbygoogle = window.adsbygoogle || []).push({});
+
 export default Ads;
--- src/Ads2.js
+++
@@ -1,6 +0,0 @@
-function Ads2(){
-  return(
-    (adsbygoogle = window.adsbygoogle || []).push({});
-  )
-}
-export default Ads2;
