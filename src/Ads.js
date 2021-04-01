8fd777e435da89debf40150f2225b41eac6bacf0
Michell Zipitría <mzipi@outlook.com>
2021-03-31 22:57:12 -0300

    atom

--- src/Ads.js
+++ src/Ads.js
@@ -3,11 +3,8 @@ import { Adsense } from '@ctrl/react-adsense';
 function Ads(){
     return(
             <Adsense
-                client="ca-pub-7640562161899788"
-                slot="7259870550"
-                style={{ display: 'block' }}
-                layout="in-article"
-                format="fluid"
+                client="ca-pub-1276989055234826"
+                slot="7778877281"
             />
     )
 }
