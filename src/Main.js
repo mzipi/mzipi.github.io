f2f5e0040cae92da6431010ea39da64674b89b7b
Michell Zipitría <mzipi@outlook.com>
2021-04-01 01:37:18 -0300

    ads

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,16 @@
 import Juego1 from './Juego1';
 import Tecno1 from './Tecno1';
 import Juego2 from './Juego2';
-import Ads from './Ads';
 
 function Main(){
   return(
     <div className="main">
       <main>
-        <div>
           <div>
-            <Ads />
+            <Juego1 />
+            <Tecno1 />
+            <Juego2 />
           </div>
-            <div>
-              <Juego1 />
-              <Tecno1 />
-              <Juego2 />
-            </div>
-        </div>
       </main>
     </div>
   )
